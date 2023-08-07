499845ccb7e191f5df8ee7a99d37d37eeab0a6aa
anjlirathi <anjalirathi987@gmail.com>
2023-08-07 20:31:05 +0530

    uploaded new tables

--- churnanalysis.sql
+++ churnanalysis.sql
@@ -6,3 +6,9 @@ SELECT customer_id, age, gender
 FROM `serious-sql-394805.Churn_Analysis123.Telecom_Churn_Analysis`
 Where age > 25 and Gender = 'Female'
 limit 5;
+
+SELECT * FROM  `serious-sql-394805.Churn_Analysis123.telecom_data_dictionary` 
+;
+
+SELECT * FROM  `serious-sql-394805.Churn_Analysis123.telecom_zip_population` 
+LIMIT 10;
